9723acd311026ad408f2e98dd94e2b3a3902c708
solid <solidsrv@gmail.com>
2020-04-26 22:09:46 +0200

    [FIX] OAuthData json names

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const OAuth2Data = require('./google_key.json')
 
 const app = express()
 
-const CLIENT_ID = OAuth2Data.client_id;
-const CLIENT_SECRET = OAuth2Data.client_secret;
-const REDIRECT_URL = OAuth2Data.client_redirect
+const CLIENT_ID = OAuth2Data.web.id;
+const CLIENT_SECRET = OAuth2Data.web.secret;
+const REDIRECT_URL = OAuth2Data.web.redirect
 
 const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL)
 var authed = false;
