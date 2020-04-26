751dd9b058f9347a07678945855b97781db76e41
solid <solidsrv@gmail.com>
2020-04-26 21:54:10 +0200

    Authentication with Google in NodeJS - tutorial

--- app.js
+++ app.js
@@ -1,10 +1,62 @@
-var express = require('express');
-var app = express();
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+const { google } = require('googleapis');
+const express = require('express')
+const OAuth2Data = require('./google_key.json')
+
+const app = express()
+
+const CLIENT_ID = OAuth2Data.client.id;
+const CLIENT_SECRET = OAuth2Data.client.secret;
+const REDIRECT_URL = OAuth2Data.client.redirect
+
+const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL)
+var authed = false;
+
+app.get('/', (req, res) => {
+    if (!authed) {
+        // Generate an OAuth URL and redirect there
+        const url = oAuth2Client.generateAuthUrl({
+            access_type: 'offline',
+            scope: 'https://www.googleapis.com/auth/gmail.readonly'
+        });
+        console.log(url)
+        res.redirect(url);
+    } else {
+        const gmail = google.gmail({ version: 'v1', auth: oAuth2Client });
+        gmail.users.labels.list({
+            userId: 'me',
+        }, (err, res) => {
+            if (err) return console.log('The API returned an error: ' + err);
+            const labels = res.data.labels;
+            if (labels.length) {
+                console.log('Labels:');
+                labels.forEach((label) => {
+                    console.log(`- ${label.name}`);
+                });
+            } else {
+                console.log('No labels found.');
+            }
+        });
+        res.send('Logged in')
+    }
+})
+
+app.get('/auth/google/callback', function (req, res) {
+    const code = req.query.code
+    if (code) {
+        // Get an access token based on our OAuth code
+        oAuth2Client.getToken(code, function (err, tokens) {
+            if (err) {
+                console.log('Error authenticating')
+                console.log(err);
+            } else {
+                console.log('Successfully authenticated');
+                oAuth2Client.setCredentials(tokens);
+                authed = true;
+                res.redirect('/')
+            }
+        });
+    }
 });
 
-const port = process.env.PORT || 3000
-app.listen(port, function () {
-  console.log(`Example app listening on port ${port}`);
-});
+const port = process.env.port || 5000
+app.listen(port, () => console.log(`Server running at ${port}`));
