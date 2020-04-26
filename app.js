dbd270886c54d11a2f8ef586d36d2a72f2c28b1d
solid <solidsrv@gmail.com>
2020-04-26 21:43:44 +0200

    port problem fix

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var app = express();
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+
+const port = process.env.PORT || 3000
+app.listen(port, function () {
+  console.log(`Example app listening on port ${port}`);
 });
